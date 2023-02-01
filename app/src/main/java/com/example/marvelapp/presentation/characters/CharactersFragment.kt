b402fe9649e25ca15be749a5d3124dc0a7a505d1
Gabriel Hora <gabrielhoradeveloper@gmail.com>
2023-01-31 23:18:30 -0300

    init app

---
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -0,0 +1,54 @@
+package com.example.marvelapp.presentation.characters
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.core.domain.model.Character
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class CharactersFragment : Fragment() {
+
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding: FragmentCharactersBinding get() = _binding!!
+
+    private val viewModel: CharactersViewModel by viewModels()
+
+    private val charactersAdapter = CharactersAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentCharactersBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharactersAdapter()
+
+        lifecycleScope.launch {
+            viewModel.charactersPagingData("").collect { pagingData ->
+                charactersAdapter.submitData(pagingData)
+            }
+        }
+    }
+
+    private fun initCharactersAdapter() {
+        with(binding.recyclerCharacters) {
+            setHasFixedSize(true)
+            adapter = charactersAdapter
+        }
+    }
+}
